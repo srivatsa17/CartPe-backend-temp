80ca694d2621b2a165818ccf60e4ded29103fba9
srivatsa <srivatsa1199@gmail.com>
2021-09-24 01:28:11 +0530

    added database values in settings.py

--- Django_Ecom/Django_Ecom/settings.py
+++ Django_Ecom/Django_Ecom/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'Django_Ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
