55387eb7ef64f16dad2ab057002e5eaf6db81fab
srivatsa <srivatsa1199@gmail.com>
2021-09-23 19:51:31 +0530

    added .env file to .gitignore

--- Django_Ecom/Django_Ecom/settings.py
+++ Django_Ecom/Django_Ecom/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
